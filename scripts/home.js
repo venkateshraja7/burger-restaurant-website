f077b2110194405ca365d4187c37b3e9f66161cb
venkateshraja7 <125277273+venkateshraja7@users.noreply.github.com>
2024-11-15 21:29:49 +0530

    Update home.js

--- scripts/home.js
+++ scripts/home.js
@@ -20,18 +20,6 @@ starters.push({
     cost: "6.00$"
 });
 
-starters.push({
-    name: "Starter 4",
-    composition: "ingredient, ingredient, ingredient, ingredient",
-    cost: "8.00$"
-});
-
-starters.push({
-    name: "Starter 5",
-    composition: "ingredient, ingredient, ingredient, ingredient",
-    cost: "12.00$"
-});
-
 /* ----------------------------MAIN COURSES---------------------------- */
 let mainCourses = [];
 
@@ -65,66 +53,32 @@ mainCourses.push({
     cost: "13.00$"
 });
 
-mainCourses.push({
-    name: "Main Course 6",
-    composition: "ingredient, ingredient, ingredient, ingredient, ingredient, ingredient, ingredient, ingredient",
-    cost: "17.00$"
-});
-
-mainCourses.push({
-    name: "Main Course 7",
-    composition: "ingredient, ingredient, ingredient, ingredient, ingredient, ingredient, ingredient, ingredient",
-    cost: "17.00$"
-});
-
-mainCourses.push({
-    name: "Main Course 8",
-    composition: "ingredient, ingredient, ingredient, ingredient, ingredient, ingredient, ingredient, ingredient",
-    cost: "15.00$"
-});
-
-mainCourses.push({
-    name: "Main Course 9",
-    composition: "ingredient, ingredient, ingredient, ingredient, ingredient, ingredient, ingredient, ingredient",
-    cost: "18.00$"
-});
-
-mainCourses.push({
-    name: "Main Course 10",
-    composition: "ingredient, ingredient, ingredient, ingredient, ingredient, ingredient, ingredient, ingredient",
-    cost: "22.00$"
-});
-
 /* ------------------------------DESSERTS------------------------------ */
 let desserts = [];
 
 desserts.push({
-    name: "Dessert 1",
+    name: "Coconut Cake ",
     composition: "ingredient, ingredient, ingredient, ingredient",
     cost: "7.00$"
 });
 
 desserts.push({
-    name: "Dessert 2",
-    composition: "ingredient, ingredient, ingredient, ingredient",
+    name: "Flan ",
     cost: "8.00$"
 });
 
 desserts.push({
-    name: "Dessert 3",
-    composition: "ingredient, ingredient, ingredient, ingredient",
+    name: "Gelato ",
     cost: "8.00$"
 });
 
 desserts.push({
-    name: "Dessert 4",
-    composition: "ingredient, ingredient, ingredient, ingredient",
+    name: "Lemon Tart ",
     cost: "12.00$"
 });
 
 desserts.push({
-    name: "Dessert 5",
-    composition: "ingredient, ingredient, ingredient, ingredient",
+    name: "Linzer Torte ",
     cost: "15.00$"
 });
 
@@ -132,111 +86,77 @@ desserts.push({
 let drinks = [];
 
 drinks.push({
-    name: "Drink 1",
-    composition: "ingredient, ingredient, ingredient",
+    name: "Manago Juice",
     cost: "2.00$"
 });
 
 drinks.push({
-    name: "Drink 2",
-    composition: "ingredient, ingredient, ingredient",
+    name: "pineapple juice",
     cost: "2.00$"
 });
 
 drinks.push({
-    name: "Drink 3",
-    composition: "ingredient, ingredient, ingredient",
+    name: "pear juice ",
     cost: "3.00$"
 });
 
 drinks.push({
-    name: "Drink 4",
-    composition: "ingredient, ingredient, ingredient",
+    name: "Carrot orange juice",
     cost: "3.00$"
 });
 
 drinks.push({
-    name: "Drink 5",
-    composition: "ingredient, ingredient, ingredient",
+    name: "pomegranate citrus juice",
     cost: "4.00$"
-});
-
-drinks.push({
-    name: "Drink 6",
-    composition: "ingredient, ingredient, ingredient",
-    cost: "5.00$"
 });
 
 /* ------------------------------ALCOHOLS------------------------------ */
 let alcohols = [];
 
 alcohols.push({
-    name: "Alcohol 1",
-    composition: "ingredient, ingredient, ingredient, ingredient",
+    name: "Old Fashioned ",
     cost: "7.00$"
 });
 
 alcohols.push({
-    name: "Alcohol 2",
-    composition: "ingredient, ingredient, ingredient, ingredient",
+    name: "Negroni ",
     cost: "7.00$"
 });
 
 alcohols.push({
-    name: "Alcohol 3",
-    composition: "ingredient, ingredient, ingredient, ingredient",
+    name: "Daiquiri ",
     cost: "7.00$"
 });
 
 alcohols.push({
-    name: "Alcohol 4",
-    composition: "ingredient, ingredient, ingredient, ingredient",
+    name: "Dry Martini ",
     cost: "8.00$"
 });
 
 alcohols.push({
-    name: "Alcohol 5",
-    composition: "ingredient, ingredient, ingredient, ingredient",
+    name: "Margarita ",
     cost: "8.00$"
-});
-
-alcohols.push({
-    name: "Alcohol 6",
-    composition: "ingredient, ingredient, ingredient, ingredient",
-    cost: "10.00$"
-});
-
-alcohols.push({
-    name: "Alcohol 7",
-    composition: "ingredient, ingredient, ingredient, ingredient",
-    cost: "15.00$"
-});
-
-alcohols.push({
-    name: "Alcohol 8",
-    composition: "ingredient, ingredient, ingredient, ingredient",
-    cost: "25.00$"
 });
 
 /* -------------------------------------------REVIEWS------------------------------------------ */
 let reviews = [];
 
 reviews.push({
-    name: "Name 1",
+    name: "Ajith Kumar",
     text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
     imageURL: "./images/person1.jpg"
     /* Photo by https://unsplash.com/@vhladynets */
 });
 
 reviews.push({
-    name: "Name 2",
+    name: "Shalini",
     text: "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages.",
     imageURL: "./images/person2.jpg"
     /* Photo by https://unsplash.com/@cikstefan */
 });
 
 reviews.push({
-    name: "Name 3",
+    name: "NAnoushka ",
     text: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.",
     imageURL: "./images/person3.jpg"
     /* Photo by https://unsplash.com/@juricakoletic */
